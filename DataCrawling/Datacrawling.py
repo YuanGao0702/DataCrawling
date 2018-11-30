b3d2bd175dfff8a59b1c581818eae0b07588ccfa
Gary Gao <yuan.g.gao@foxconn.com>
2018-11-29 20:04:34 -0500

    change

--- DataCrawling/Datacrawling.py
+++ DataCrawling/Datacrawling.py
@@ -2,6 +2,11 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import requests  
+from selenium.webdriver.common.by import By
+import selenium as se
+import sys 
 
 def simple_get(url):
 	"""
@@ -21,7 +26,7 @@ def simple_get(url):
 		return None
 		
 def is_good_response(resp):
-	"""
+	""" 
     Returns True if the response seems to be HTML, False otherwise.
     """
 	content_type = resp.headers['Content-Type'].lower()
@@ -38,19 +43,40 @@ def log_error(e):
     """
     print(e)
 
-raw_html = simple_get('https://www.w3schools.com/python/python_getstarted.asp')
-html = BeautifulSoup(raw_html, 'html.parser')
-print(len(html))
-flag = '0'
-result = []
-for a in html.select('a'):
-	if a.text == 'Python HOME':
-		flag = '1'
-	if flag == '1':
-		result.append(a.text);
-	if a.text == 'Python Try...Except':
-		break
-		
-print('Python Tag')
-for tag in result:
-	print(tag)
+def learnpython():
+	raw_html = simple_get('https://www.w3schools.com/python/python_getstarted.asp')
+	soup  = BeautifulSoup(raw_html, 'html.parser') 
+	flag = '0'
+	result = []
+	for a in soup.select('a'):
+		if a.text == 'Python HOME':
+			flag = '1'
+		if flag == '1':
+			result.append(a.text);
+		if a.text == 'Python Try...Except':
+			break
+			
+	print('Python Tag')
+	for tag in result:
+		print(tag)
+
+def sel():
+	#driver = webdriver.Chrome()
+	driver = webdriver.PhantomJS()
+	url = 'https://translate.google.com/#view=home&op=translate&sl=en&tl=zh-CN&text=aaaaaaa'
+	driver.get(url)
+	element = driver.find_element(By.XPATH,'//span[@class="tlid-translation translation"]')
+	print (element.text) 
+
+def sel2(str):
+	options = se.webdriver.ChromeOptions()
+	options.add_argument('headless') 
+	driver = se.webdriver.Chrome(chrome_options=options)
+	url = 'https://translate.google.com/#view=home&op=translate&sl=en&tl=zh-CN&text='+str
+	print(url)
+	driver.get(url)
+	element = driver.find_element(By.XPATH,'//span[@class="tlid-translation translation"]') 
+	element2 = driver.find_elements(By.XPATH,'//div[@class="tlid-transliteration-content transliteration-content full"]')
+	print (element.text)  
+ 
+sel2(sys.argv[1])
